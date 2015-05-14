ff97dd455d52cce584914fc7990c725d76c464c5
chris.hou <chrishou118@gmail.com>
2015-05-14 05:03:01 -0700

    0226 regular expression

---
+++ 0226/bingo.js
@@ -0,0 +1,68 @@
+window.onload = initAll;
+
+function initAll() {
+  document.getElementById("reload").onclick = anotherCard;
+  newCard();
+}
+
+function newCard() {
+  for (var i = 0; i < 24; i++) {
+    setSquare(i);
+  }
+}
+
+var colPlace = new Array(0, 0, 0, 0, 0,  // B
+                         1, 1, 1, 1, 1,  // I
+                         2, 2, 2, 2,     // N
+                         3, 3, 3, 3, 3,  // G
+                         4, 4, 4, 4, 4); // O
+
+var usedNums = new Array(76);
+
+function setSquare(thisSquare)
+{
+    var currSquare = "square" + thisSquare;
+    var colBasis = colPlace[thisSquare]*15;
+    var newNum;
+
+    do {
+        newNum = colBasis + getNewNum() + 1;
+    } while (usedNums[newNum]);
+
+    usedNums[newNum] = true;
+    document.getElementById(currSquare).innerHTML = newNum;
+    document.getElementById(currSquare).className = "";
+    document.getElementById(currSquare).onmousedown = toggleColor;
+}
+
+function getNewNum()
+{
+    return Math.floor(Math.random()*15);
+}
+
+function anotherCard()
+{
+    for (var i = 1; i < usedNums.length; i++) {
+        usedNums[i] = false;
+    }
+
+    newCard();
+    return false;
+}
+
+function toggleColor(evt)
+{
+    if (evt) {
+        var thisSquare = evt.target;
+    }
+    else {
+        var thisSquare = window.event.srcElement;
+    }
+
+    if (thisSquare.className == "") {
+        thisSquare.className = "pickedBG";
+    }
+    else {
+        thisSquare.className = "";
+    }
+}
